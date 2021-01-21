9a89860fab5bacffdc8b2be67283d21888ce460e
MoonSam <32975650+MoonSam@users.noreply.github.com>
2021-01-22 01:08:00 +0900

    test

---
+++ single_cycle_cpu.sv
@@ -0,0 +1,146 @@
+/* ********************************************
+ *	COSE222 Lab #3
+ *
+ *	Module: top design of the single-cycle CPU (single_cycle_cpu.sv)
+ *  - Top design of the single-cycle CPU
+ *
+ *  Author: Gunjae Koo (gunjaekoo@korea.ac.kr)
+ *
+ * ********************************************
+ */
+
+`timescale 1ns/1ps
+`define FF 1    // Flip-flop delay for just better waveform view
+
+module single_cycle_cpu
+#(  parameter IMEM_DEPTH = 1024,    // imem depth (default: 1024 entries = 4 KB)
+              IMEM_ADDR_WIDTH = 10,
+              REG_WIDTH = 64,
+              DMEM_DEPTH = 1024,    // dmem depth (default: 1024 entries = 8 KB)
+              DMEM_ADDR_WIDTH = 10 )
+(
+    input           clk,            // System clock
+    input           reset_b         // Asychronous negative reset
+);
+
+    // Wires for datapath elements
+    logic   [IMEM_ADDR_WIDTH-1:0]   imem_addr;
+    logic   [31:0]  inst;   // instructions = an output of ????
+
+    logic   [4:0]   rs1, rs2, rd;    // register numbers
+    logic   [REG_WIDTH-1:0] rd_din;
+    logic           reg_write;
+    logic   [REG_WIDTH-1:0] rs1_dout, rs2_dout;
+
+    logic   [REG_WIDTH-1:0] alu_in1, alu_in2;
+    logic   [3:0]   alu_control;    // ALU control signal
+    logic   [REG_WIDTH-1:0] alu_result;
+    logic           alu_zero;
+
+    logic   [DMEM_ADDR_WIDTH-1:0]    dmem_addr;
+    logic   [63:0]  dmem_din, dmem_dout;
+    logic           mem_read, mem_write;
+
+    // -------------------------------------------------------------------
+    /* Main control unit:
+     * Main control unit generates control signals for datapath elements
+     * The control signals are determined by decoding instructions
+     * Generating control signals using opcode = inst[6:0]
+     */
+    logic   [6:0]   opcode;
+    logic           branch, alu_src, mem_to_reg;
+    logic   [1:0]   alu_op;
+    //logic         mem_read, mem_write, reg_write; // declared above
+
+    // COMPLETE THE MAIN CONTROL UNIT HERE
+
+
+
+    // --------------------------------------------------------------------
+
+    // --------------------------------------------------------------------
+    /* ALU control unit:
+     * ALU control unit generate alu_control signal which selects ALU operations
+     * Generating control signals using alu_op, funct7, and funct3 fileds
+     */
+    logic   [6:0]   funct7;
+    logic   [2:0]   funct3;
+
+    // COMPLETE THE ALU CONTROL UNIT HERE
+
+
+
+    // ---------------------------------------------------------------------
+
+
+    // ---------------------------------------------------------------------
+    /* Immediate generator:
+     * Generating immediate value from inst[31:0]
+     */
+    logic   [63:0]  imm64;
+    logic   [63:0]  imm64_branch;  // imm64 left shifted by 1
+    logic   [11:0]  imm12;  // 12-bit immediate value extracted from inst
+
+    // COMPLETE IMMEDIATE GENERATOR HERE
+
+
+
+    // ----------------------------------------------------------------------
+
+    // Program counter
+    logic   [63:0]  pc_curr, pc_next;
+    logic   [63:0]  pc_next_plus4, pc_next_branch;
+
+    assign pc_next_plus4 = ;    // FILL THIS
+
+    always_ff @ (posedge clk or negedge reset_b) begin
+        if (~reset_b) begin
+            pc_curr <= 'b0;
+        end else begin
+            pc_curr <= ;        // FILL THIS
+        end
+    end
+
+
+    // MUXes:
+    // COMPLETE MUXES HERE
+    // PC_NEXT
+    assign pc_next_branch = ;   // FILL THIS
+
+    // ALU inputs
+
+
+
+    // RF din
+
+
+    // COMPLETE CONNECTIONS HERE
+    // imem
+
+    // regfile
+ 
+    // dmem
+
+
+
+    // -----------------------------------------------------------------------
+    /* Instantiation of datapath elements
+     * All input/output ports should be connected
+     */
+    
+    // IMEM
+    imem #(
+        .IMEM_DEPTH         (IMEM_DEPTH),
+        .IMEM_ADDR_WIDTH    (IMEM_ADDR_WIDTH)
+    ) u_imem_0 (
+        .addr               ( imem_addr     ),
+        .dout               (               )
+    );
+
+    // REGFILE
+
+    // ALU
+
+    // DMEM
+
+endmodule
